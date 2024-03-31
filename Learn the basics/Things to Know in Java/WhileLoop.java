b31d2fa1d37078e6b2fa38c5000d86d5dd2d8a7b
krishnapatidar458 <kk7patidardudawat@gmail.com>
2024-03-31 11:04:59 +0530

    basic While loop using java

---
+++ Learn the basics/Things to Know in Java/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop{
+    public static void main(String[] krishna){
+        int i=1; // initialization of loop control variable
+        while(i<=7){ // condition to continue the loop
+            System.out.println(i); // prints numbers from 1 to 7
+            i++; // incrementation of loop control variable
+        }
+    }
+}
