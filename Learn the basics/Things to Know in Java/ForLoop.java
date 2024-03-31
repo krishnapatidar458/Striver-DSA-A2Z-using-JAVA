609420f82607563db819bbe8afcf335d9d84e4f1
krishnapatidar458 <kk7patidardudawat@gmail.com>
2024-03-31 11:02:06 +0530

    basic for loop using java

---
+++ Learn the basics/Things to Know in Java/ForLoop.java
@@ -0,0 +1,7 @@
+class ForLoop{
+    public static void main(String[] args){
+        for (int i =1; i <=5 ;i++){  //for loop starts from 0 and ends at 4 because the condition is i<5.
+            System.out.println("Hello krishnapatidar458! : "+i);
+        }
+    }
+}
