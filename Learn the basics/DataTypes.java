27b57c4724d6bd3df2238f7e2aeaffd7f82739d2
krishnapatidar458 <kk7patidardudawat@gmail.com>
2024-03-31 10:16:15 +0530

    primitive datatyoes

---
+++ Learn the basics/DataTypes.java
@@ -0,0 +1,20 @@
+class DataTypes{
+    public static void main(String args[]){
+        int  x = 24;        //integer data type
+        float  y = 23.7f;   //float data type
+        boolean z = false;  //boolean data type which can have only two values true or false
+        char c = 'a';       //character data type it stores a single character
+        byte b = 101;       //byte is an 8-bit signed integer
+        short s = 3452;     //short is a 16-bit signed integer
+        double  d = 99.99d;  //double is a double precision floating point number
+        long  l = 123456L;  //long is a 64-bit signed integer
+        System.out.println("Integer : " +x);
+        System.out.println("Float : "  +y);
+        System.out.println("Boolean : "+z);
+        System.out.println("Character : " +c);
+        System.out.println("Byte : "  +b);
+        System.out.println( "Short : " +s);
+        System.out.println("Double : " +d);
+        System.out.println("Long  : " +l);
+    }
+}
