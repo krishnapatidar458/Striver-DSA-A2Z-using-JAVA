36a930deaee0b6fd8ef15a95d8e004556c41d828
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-04-21 09:40:25 +0530

    changes

---
+++ Learn the basics/Stage3 - Java Collection Frameworks/ArrList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+class ArrList{
+    public static void main(String[] args){
+        ArrayList<Integer> list  = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list.add(0);
+        list.add(2);
+        list.add(3);
+        // System.out.println(list);
+        list.add(1,1);
+        // System.out.println(list);
+        // list2.add(11);
+        // list2.add(12);
+        // System.out.println(list2);
+        // list.addAll(0,list2);
+        // System.out.println(list);
+        list.remove
+    }
+}
