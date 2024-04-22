9e01c00aaa9b6d8b0a95e025a90331e6a39b0221
krishnapatidar458 <kp7patidardudawat@gmail.com>
2024-04-22 13:53:14 +0530

    update linkedList using queue

--- Learn the basics/Stage3 - Java Collection Frameworks/queue.java
+++ Learn the basics/Stage3 - Java Collection Frameworks/queue.java
@@ -1,8 +1,10 @@
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Collection;
 class queue{
     public static void main(String args[]){
         Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q1 = new LinkedList<>();
         q.offer(10);
         q.offer(20);
         q.offer(30);
@@ -10,8 +12,12 @@ class queue{
         q.offer(50);
         System.out.println(q);
         q.poll();
+        q1.offer(12);
+        q1.offer(13);
+        q1.offer(14);
+        q.addAll(q1);
         System.out.println(q);
         System.out.println(q.peek());
-        
+
     }
 }
